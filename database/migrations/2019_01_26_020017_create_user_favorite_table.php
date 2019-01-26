4fdf05a37d3175012d14e4c3d3abe7ee26e18d7d
tetsu.yamaguchi <treekunbe9@gmail.com>
2019-01-26 02:32:13 +0000

    Procfile

---
+++ database/migrations/2019_01_26_020017_create_user_favorite_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserFavoriteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    
+    public function up()
+    {
+        Schema::create('user_favorite', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('favorite_id')->unsigned()->index();
+            $table->timestamps();
+
+            // 外部キー設定
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('favorite_id')->references('id')->on('tasks')->onDelete('cascade');
+
+            // user_idとfollow_idの組み合わせの重複を許さない
+            $table->unique(['user_id', 'favorite_id']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('user_favorite');
+    }
+}
